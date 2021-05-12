43ce2470792190aa45d6af2b1345a157944fe156
Yan Almeida <yanalmeidagarcia@gmail.com>
2021-05-11 21:22:34 -0300

    feat: adição de home

---
+++ src/views/home/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../../layout';
+import { Title } from './styles';
+
+const Home = () => {
+  return (
+    <Layout>
+      <Title>Home</Title>
+    </Layout>
+  );
+};
+
+export default Home;
